0b865db1f1c1826442b7271b4ef1b46aa1078326
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-04-06 22:45:39 -0700

    added user login , added user password salt & hash

--- application/server/models/user.js
+++ application/server/models/user.js
@@ -1,13 +1,38 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 
 var UserSchema = new mongoose.Schema({
-  email: String,  
+  email: {type: String, required: true, index:{ unique: true }},  
   username: String,
   name: String,
-  password: String,
+  password: {type: String, required: true}
 },{
   timestamps: true
 });
+
+UserSchema.pre('save',function(next) {
+	var user = this;
+
+	if(!user.isModified('password')) return next();
+
+	bcrypt.genSalt(10,function(err, salt){
+		if(err) return next(err);
+
+		bcrypt.hash(user.password,salt,function(err, hash){
+		  if(err) return next(err);
+		  user.password = hash;
+		  next();
+		})
+	});
+});
+
+UserSchema.methods.comparePassword = function(password, cb) {
+    bcrypt.compare(password, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+ 
 
 module.exports = mongoose.model('User', UserSchema);
 
--- application/server/routes/user-router.js
+++ application/server/routes/user-router.js
@@ -4,21 +4,47 @@ var User = require('../models/user');
 
 router.get('/all', (req, res) => {
   User.find({}, 'username name email').select('-_id').exec(function( err, users){
-  	if(err){console.log(err);}
-  	else{
-  	 res.json(users);
-  	}
+  	if(err){
+      console.log(err);
+      return;
+    }
+    
+    if(users.length){
+  	  res.json(users);
+    }else{
+      res.status = 404;
+    }
+  	
   });
 });
 
 router.post('/login', (req, res) => {
-
-
+  User.findOne({ email: req.body.email },function(err, user){
+     if(err){
+      console.log(err);
+      return;
+     }
+     
+     if(user){      
+        user.comparePassword(req.body.password, function(err, isMatch) {
+          console.log(isMatch);
+          if(isMatch == true){
+            console.log("logged in");
+          }else {
+           console.log("failed login");
+          }
+        });
+     }
+     else{
+      console.log("user not found");
+      res.status = 404;
+     }
+  });
 });
 
 
 router.post('/new', (req, res) => {
- 
+
   new User(req.body.registrationForm).save(function(err){
      
       //if post success , else failure
@@ -31,8 +57,6 @@ router.post('/new', (req, res) => {
    	  }
      res.send();
   });
-
-  res.send();
 });
 
 module.exports = router;
