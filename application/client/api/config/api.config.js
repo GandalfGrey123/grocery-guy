a78def9edab382c105cf842f9483f9b6d96861a3
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-04-22 03:58:38 -0700

    Update api.config.js

--- application/client/api/config/api.config.js
+++ application/client/api/config/api.config.js
@@ -1,6 +1,7 @@
 const config = () => {
   let environment = {
     'environment': 'localhost:5000',
+    'registration-url': 'localhost:5000/user/register-form-show',
   };
  return environment;
 };
