411a6903deeb2d3ba132db6f81a69de261e2cb19
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-04-22 04:00:53 -0700

    added registration browser view

--- application/server/app.js
+++ application/server/app.js
@@ -2,28 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 var bodyParser = require('body-parser')
 var app = express();
-
 var pjson = require('./package.json');
 const userRouter = require('./routes/user-router');
 
 
 const dbUrl = 'mongodb://localhost:27017/grocery-guy';
-
-const serverStart = () => {
-	  app.use(bodyParser.urlencoded({
-      extended: true
-    }));
-    app.use(bodyParser.json());
-	  app.use('/user', userRouter);
-   
-    app.listen(5000, '127.0.0.1', () => {
-        console.log('the ' + pjson.name + ' server is listening on 127.0.0.1:5000');
-    });
-}
-
 mongoose.connect(dbUrl,{ useNewUrlParser: true })
   .then(() =>  console.log(dbUrl + 'connection succesful!'))
   .catch((err) => console.error(err));
+   
+app.set(`view engine`, `ejs`);
 
 
-serverStart();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());    
+app.use('/user', userRouter);
+
+app.listen(5000, '127.0.0.1', () => {
+    console.log('the ' + pjson.name + ' server is listening on 127.0.0.1:5000');
+});
+
