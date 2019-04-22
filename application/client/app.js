94fd1dcacb08e5357e444fcadc4973b378dadeb2
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-04-22 03:59:46 -0700

    Update app.js

--- application/client/app.js
+++ application/client/app.js
@@ -1,12 +1,12 @@
 const {app,BrowserWindow} = require('electron');
+const ejsEngine = require('ejs-electron');
 
 let win
-
 
 function loadHomePage(){
 	win = new BrowserWindow({width:800,height:600});
   win.webContents.openDevTools()
-	win.loadFile('./views/homepage/index.html');
+	win.loadFile('./views/pages/index.ejs');
 	win.on('closed', () => {
      win = null
   })
@@ -14,7 +14,6 @@ function loadHomePage(){
 
 
 // mac OS specific events
-
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
