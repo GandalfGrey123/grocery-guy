833e5c3025180053cfe87a85b1c966d0f0fe572c
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-04-26 01:22:39 -0700

    fix warning
    DeprecationWarning: collection.ensureIndex

--- application/server/app.js
+++ application/server/app.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
-var bodyParser = require('body-parser')
-var app = express();
+var bodyParser = require('body-parser');
+
 var pjson = require('./package.json');
 const userRouter = require('./routes/user-router');
+const models = require('./models/user')
 
+var app = express();
 
 const dbUrl = 'mongodb://localhost:27017/grocery-guy';
-mongoose.connect(dbUrl,{ useNewUrlParser: true })
-  .then(() =>  console.log(dbUrl + 'connection succesful!'))
-  .catch((err) => console.error(err));
+mongoose.connect( dbUrl,{ 
+	  useNewUrlParser: true,
+	  useFindAndModify: false,
+	  useCreateIndex: true,
+	})
+  	 .then(() =>  console.log(dbUrl + 'connection succesful!'))
+  	 .catch((err) => console.error(err));
    
 app.set(`view engine`, `ejs`);
-
 
 app.use(bodyParser.urlencoded({
   extended: true
