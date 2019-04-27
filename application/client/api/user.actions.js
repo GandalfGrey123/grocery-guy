d42dc2d9fd4d98cb0aa8dbf9758d620ccb74e910
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-04-27 04:57:29 -0700

    Update user.actions.js

--- application/client/api/user.actions.js
+++ application/client/api/user.actions.js
@@ -1,1 +1,9 @@
 //logged in actions
+var request = require('request');
+var api_config = require('./config/api.config');
+
+
+let mainSearchButton = document.getElementById("search");
+mainSearchButton.addEventListener('click', function(event) {
+	event.preventDefault();
+});
