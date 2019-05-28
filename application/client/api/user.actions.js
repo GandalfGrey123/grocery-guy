8d68139cd305e57c092d1ea55d792a2348169f3a
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-05-27 20:37:01 -0700

    clean up

--- application/client/api/user.actions.js
+++
@@ -1,9 +0,0 @@
-//logged in actions
-var request = require('request');
-var api_config = require('./config/api.config');
-
-
-let mainSearchButton = document.getElementById("search");
-mainSearchButton.addEventListener('click', function(event) {
-	event.preventDefault();
-});
