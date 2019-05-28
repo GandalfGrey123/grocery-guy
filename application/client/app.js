121f7f3077482cfd234b94333b39a333cff83dcb
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-05-27 20:38:29 -0700

    cleanup

--- application/client/app.js
+++ application/client/app.js
@@ -1,46 +1,37 @@
-const {app,BrowserWindow} = require('electron');
+const {app,BrowserWindow,ipcMain} = require('electron');
 const ejsEngine = require('ejs-electron');
-
-const request = require('request');
-var api_config = require('./api/config/api.config');
 
 let win
 
-function loadHomePage(){
-
+loadHomePage = () => {
 	win = new BrowserWindow({width:800,height:600});
-  win.webContents.openDevTools()
-  
+  win.webContents.openDevTools()  
   win.loadFile('./views/pages/index.ejs');
-
   win.on('closed', () => {
      win = null
   })
 }
 
-
-
-/////  MAC EVENTS
-// mac OS specific events
-// might need to kill the cookie & session here if possible
+//when to kill app
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-
-//not sure if the cookie dies during this event
-//not sure if i need to call start session again
+//event when app closed but still active in doc
 app.on('activate', () => {
-   // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     loadHomePage() 
   }
 })
-/////  MAC EVENTS END
-
 
 
 app.on('ready', loadHomePage)
+
+ipcMain.on('open-new-window',() =>{
+ win.close()
+ win = new BrowserWindow({width:800,height:600});
+ win.webContents.openDevTools()
+ win.loadFile('./views/pages/index.ejs');
+});
