76f1f04cbdd7686d4275082dd357f8b755f7d231
Marcus <gandalfgrey@gmx.com>
2019-06-06 19:53:52 -0700

    initialize login page component

---
+++ application/client/src/modules/login/LoginPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button,} from 'react-bootstrap';
+
+
+class LoginPage extends Component{
+
+    render(){
+      return(
+        <div>
+
+        </div>
+      );
+    }
+}
+export default (LoginPage);
