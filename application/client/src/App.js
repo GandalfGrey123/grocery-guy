2ef9b5d0dab8819784dfc389fb5b2d4b3ec5f8d8
Marcus <gandalfgrey@gmx.com>
2019-06-06 19:52:10 -0700

    added react and react-bootstrap

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LoginPage from './modules/login/LoginPage';
+import "react-bootstrap/dist/react-bootstrap.min.js";
 
 class App extends Component {
 
   render() {      
     return (
-      <div></div>
+      <div>
+        <BrowserRouter>
+        <Switch>
+          <Route path={'/'} component={LoginPage} />
+        </Switch>
+        </BrowserRouter>
+      </div>
     );
   }
 }
