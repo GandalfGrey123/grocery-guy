e663385a2905054545422e937d9009c04facd9d7
Marcus <gandalfgrey@gmx.com>
2019-06-15 00:52:07 -0700

    added unauthorized redirect
    added authorization to home page component

--- application/client/src/modules/homepage/HomePage.js
+++ application/client/src/modules/homepage/HomePage.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
-import {
-   Container, Row, Col,
-} from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
+import { authSession } from '../../api/user.actions';
 
 class HomePage extends Component {
 
-	 constructor(props){
-	   super(props);		 
+	constructor(props){
+	   super(props);
+	   this.state = {
+		authorized: true,
+	   }		 
+	 }
+
+	 componentDidMount(){		 
+	  authSession((isValid)=>{	  	
+	  	this.setState({
+		  authorized: isValid,
+	  	});
+	  });
 	 }
 
 	render(){
-	 const { loginForm } = this.state;
-	 return(
-	   <div>
-	 	 <h1> Hello World ! </h1>
-	   </div>
-	 );
+	  const{authorized} = this.state;
+
+	  if(!authorized){
+		return <Redirect to="/"/>;
+	  }
+	  
+	  return(
+	     <div>
+	       
+	     </div>
+	  );
 	}
 }
 export default (HomePage);
