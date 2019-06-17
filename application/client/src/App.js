48c5f37ff29f80a0ff893fe44598ed5a2d4cf79a
Marcus <gandalfgrey@gmx.com>
2019-06-17 10:47:18 -0700

    move global nav to app.js

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch ,Redirect} from 'react-router-dom';
 import LoginPage from './modules/login/LoginPage';
 import HomePage from './modules/homepage/HomePage';
 
-import "react-bootstrap/dist/react-bootstrap.min.js";
+import {Navbar} from 'react-bootstrap';
+
 
 class App extends Component {
 
   render() {      
     return (
       <div>
+
+        <Navbar className="p-3" expand="lg" variant="light" bg="light">
+					<Navbar.Brand href="#home">Gocery Guy version 1.0</Navbar.Brand>
+				</Navbar>
+
         <BrowserRouter>
-         <Switch>        
-           <Route path={'/Home'} component={HomePage} />
-           <Route path={'/'} component={LoginPage} />
+         <Switch>                   
+           
+           <Route path={'/Home'} component={LoginPage} />
+           <Route path={'/'} component={HomePage} />
          </Switch>
         </BrowserRouter>
       </div>
