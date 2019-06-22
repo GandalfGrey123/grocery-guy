7b09aee67598b5c593f95f074c7e3122b5617516
Marcus <gandalfgrey@gmx.com>
2019-06-21 23:28:19 -0700

    Update MealContainer.js

--- application/client/src/modules/homepage/components/MealContainer.js
+++ application/client/src/modules/homepage/components/MealContainer.js
@@ -16,39 +16,38 @@ class MealContainer extends Component {
 	  return(
 	    <div>
 
+		  <Nav className="pt-5 justify-content-center" variant="tabs" defaultActiveKey="tab-all">
+			 <Nav.Item>
+			 	<Nav.Link href="tab-all">All</Nav.Link>
+			 </Nav.Item>
 
-				  <Nav className="pt-5 justify-content-center" variant="tabs" defaultActiveKey="tab-all">
-						<Nav.Item>
-							<Nav.Link href="tab-all">All</Nav.Link>
-						</Nav.Item>
+			 <Nav.Item>
+			 	<Nav.Link eventKey="tab-break">Breakfast</Nav.Link>
+			 </Nav.Item>
 
-						<Nav.Item>
-							<Nav.Link eventKey="tab-break">Breakfast</Nav.Link>
-						</Nav.Item>
+			 <Nav.Item>
+			 	<Nav.Link eventKey="tab-lunch">Lunch</Nav.Link>
+			 </Nav.Item>
 
-						<Nav.Item>
-							<Nav.Link eventKey="tab-lunch">Lunch</Nav.Link>
-						</Nav.Item>
-
-						<Nav.Item>
-							<Nav.Link eventKey="tab-dinner"> Dinner</Nav.Link>
-						</Nav.Item>
+			 <Nav.Item>
+			 	<Nav.Link eventKey="tab-dinner"> Dinner</Nav.Link>
+			 </Nav.Item>
 						
-						<Nav.Item>
-							<Nav.Link eventKey="tab-snacks"> Snacks</Nav.Link>
-						</Nav.Item>
+			 <Nav.Item>
+			 	<Nav.Link eventKey="tab-snacks"> Snacks</Nav.Link>
+			 </Nav.Item>
 
-						<Nav.Item>
-							<Nav.Link eventKey="tab-anytime"> Anytime</Nav.Link>
-						</Nav.Item>
+			 <Nav.Item>
+			 	<Nav.Link eventKey="tab-anytime"> Anytime</Nav.Link>
+			 </Nav.Item>
 
-					</Nav>
+		  </Nav>
 
-					<Nav  
-  				 className = "navbar fixed-bottom justify-content-end"
-					>
-  		 			<Button className="btn-outline-primary" variant="link" size="lg"> + New Meal</Button>
-					</Nav>	
+		  <Nav  
+  			className = "navbar fixed-bottom justify-content-end"
+		   >
+  		   <Button className="btn-outline-primary" variant="link" size="lg"> + New Meal</Button>
+		  </Nav>	
 	       
 	    </div>
 	  );
